ca0bc5d69485280c4f76bdb4359cbf1ed35a2457
Jonathan Tsegaye <jonathantsegaye@sfs-wifi-dhcp-10-143-71-234.wifi.sfsu.edu>
2022-10-13 14:43:27 -0700

    database migration completed, ready to be deployed to render

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV === 'development') {
+  require("dotenv").config();
+}
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,9 +9,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testsRouter = require('./routes/tests');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tests', testsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +41,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
