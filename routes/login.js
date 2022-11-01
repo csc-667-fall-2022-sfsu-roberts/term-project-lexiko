dcac72b9af9f8106ed56a33f1febcfd48c48d365
Jonathan Tsegaye <jonathantsegaye@Jonathans-MacBook-Pro.local>
2022-11-01 14:36:12 -0700

    checks if user exists when loggin in

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Login' });
+router.get('/', function(request, response) {
+  response.render('login', { title: 'Login' });
 });
 
 
 
 // Send login info somewhere?
-router.post('/', function(req, res, next) {
-  console.log(req.body);
-  res.send("response sent")
+router.post('/', function(request, response) {
+  var username = request.body.username
+  console.log(username)
+  db.query(`SELECT * FROM users where username = '?'`,username)
+  .then(results => response.json(results))
+  .catch(error => {
+      console.log(error)
+      response.json({ error })
+  })
+  
 });
 
 module.exports = router;
