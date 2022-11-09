0a080cd5f094b69efe35fb3550017d08f278f984
tabadines00 <thomas.abadines@gmail.com>
2022-11-08 16:17:09 -0800

    socket stuff

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var testsRouter = require('./routes/tests');
 var registerRouter = require('./routes/register');
 var authRouter = require('./routes/auth');
 var forgotRouter = require('./routes/forgot');
+var sockettest = require('./routes/sockettest');
 
 
 var app = express();
@@ -24,11 +25,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// socket stuff
+var http = require('http').Server(app);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+/*
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
@@ -37,8 +42,18 @@ app.use(session({
   //   // Insert connect-pg-simple options here
   // })
 }));
-app.use(passport.authenticate('session'));
+*/
+ /*
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+  console.log('A user connected');
 
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+     console.log('A user disconnected');
+  });
+});
+*/
 
 app.use('/', indexRouter);
 app.use('/auth',authRouter);
@@ -47,6 +62,9 @@ app.use('/tests', testsRouter);
 // app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/forgot',forgotRouter);
+
+//socket tests
+app.use('/sockettest', sockettest);
 
 
 // catch 404 and forward to error handler
