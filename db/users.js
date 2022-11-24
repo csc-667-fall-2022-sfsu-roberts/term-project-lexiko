6d9adc6305bbd3a4ab7d68a2cc6629ac7c6e4d36
Jonathan Tsegaye <jonathantsegaye@Jonathans-MBP.lan>
2022-11-24 14:33:30 -0800

    first commit

--- db/users.js
+++ db/users.js
@@ -1,10 +1,9 @@
 const db = require("./index")
 const bcrypt = require("bcrypt");
+
 const LOOKUP_USER_BY_USERNAME = "SELECT id from users where username = ${username}";
 const REGISTER_USER = "INSERT INTO users (username, password, email) VALUES (${username}, ${password}, ${email}) RETURNING id, username";
 const LOGIN_USER = "SELECT id, username, password FROM users WHERE username=${username}";
-
-
 
 const register = ({username, password, email}) => {
    return db
@@ -12,8 +11,6 @@ const register = ({username, password, email}) => {
    .then(() => bcrypt.hash(password, 10))
    .then((hash) => db.one(REGISTER_USER, {username, password: hash, email}));
 };
-
-
 
 const login = ({ username, password }) => {
    return db
@@ -33,25 +30,4 @@ const login = ({ username, password }) => {
      });
  };
  
-
-// const login = ({ username, password }) => {
-//    return db
-//      .one(LOGIN_USER, { username })
-//      .then(({ id, username, password: encryptedPassword }) =>
-//       console.log("password")
-//       //  Promise.all([
-//       //    bcrypt.compare(password, encryptedPassword),
-//       //    { id, username },
-//       //  ])
-//      )
-//      .then(([result, { id, username }]) => {
-//        if (result) {
-//          return { id, username };
-//        } else {
-//          return Promise.reject("Please enter a valid username and password.");
-//        }
-//      });
-//  };
-
-
 module.exports = {register, login}
