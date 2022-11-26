c5ab789857c7065c888baa42aceb74f104dc137f
Radian758 <adrianvaz09@gmail.com>
2022-11-25 23:10:24 -0800

    possible footer structure

--- routes/game.js
+++ routes/game.js
@@ -13,45 +13,45 @@ function getRandom(arr, n) {
   return result;
 }
 
-var rawTileList = {
-    { "letter": "a", "points":  1, "tiles":  9 },
-    { "letter": "b", "points":  3, "tiles":  2 },
-    { "letter": "c", "points":  3, "tiles":  2 },
-    { "letter": "d", "points":  2, "tiles":  4 },
-    { "letter": "e", "points":  1, "tiles": 12 },
-    { "letter": "f", "points":  4, "tiles":  2 },
-    { "letter": "g", "points":  2, "tiles":  3 },
-    { "letter": "h", "points":  4, "tiles":  2 },
-    { "letter": "i", "points":  1, "tiles":  9 },
-    { "letter": "j", "points":  8, "tiles":  1 },
-    { "letter": "k", "points":  5, "tiles":  1 },
-    { "letter": "l", "points":  1, "tiles":  4 },
-    { "letter": "m", "points":  3, "tiles":  2 },
-    { "letter": "n", "points":  1, "tiles":  6 },
-    { "letter": "o", "points":  1, "tiles":  8 },
-    { "letter": "p", "points":  3, "tiles":  2 },
-    { "letter": "q", "points": 10, "tiles":  1 },
-    { "letter": "r", "points":  1, "tiles":  6 },
-    { "letter": "s", "points":  1, "tiles":  4 },
-    { "letter": "t", "points":  1, "tiles":  6 },
-    { "letter": "u", "points":  1, "tiles":  4 },
-    { "letter": "v", "points":  4, "tiles":  2 },
-    { "letter": "w", "points":  4, "tiles":  2 },
-    { "letter": "x", "points":  8, "tiles":  1 },
-    { "letter": "y", "points":  4, "tiles":  2 },
-    { "letter": "z", "points": 10, "tiles":  1 },
-    //" ": { "tiles": 2}
-}
+// var rawTileList = {
+//     { "letter": "a", "points":  1, "tiles":  9 },
+//     { "letter": "b", "points":  3, "tiles":  2 },
+//     { "letter": "c", "points":  3, "tiles":  2 },
+//     { "letter": "d", "points":  2, "tiles":  4 },
+//     { "letter": "e", "points":  1, "tiles": 12 },
+//     { "letter": "f", "points":  4, "tiles":  2 },
+//     { "letter": "g", "points":  2, "tiles":  3 },
+//     { "letter": "h", "points":  4, "tiles":  2 },
+//     { "letter": "i", "points":  1, "tiles":  9 },
+//     { "letter": "j", "points":  8, "tiles":  1 },
+//     { "letter": "k", "points":  5, "tiles":  1 },
+//     { "letter": "l", "points":  1, "tiles":  4 },
+//     { "letter": "m", "points":  3, "tiles":  2 },
+//     { "letter": "n", "points":  1, "tiles":  6 },
+//     { "letter": "o", "points":  1, "tiles":  8 },
+//     { "letter": "p", "points":  3, "tiles":  2 },
+//     { "letter": "q", "points": 10, "tiles":  1 },
+//     { "letter": "r", "points":  1, "tiles":  6 },
+//     { "letter": "s", "points":  1, "tiles":  4 },
+//     { "letter": "t", "points":  1, "tiles":  6 },
+//     { "letter": "u", "points":  1, "tiles":  4 },
+//     { "letter": "v", "points":  4, "tiles":  2 },
+//     { "letter": "w", "points":  4, "tiles":  2 },
+//     { "letter": "x", "points":  8, "tiles":  1 },
+//     { "letter": "y", "points":  4, "tiles":  2 },
+//     { "letter": "z", "points": 10, "tiles":  1 },
+//     //" ": { "tiles": 2}
+// }
 
 var fullTileList = new Array();
 
 let tileId = 0;
-for(const tileData of Object.entries(rawTileList)) {
-  for(let multiples = 0; multiples < tileData["tiles"]; multiples++) {
-    fullTileList[tileId] = [tileData["letter"], tileData["points"], tileId];
-    tileId++;
-  }
-}
+// for(const tileData of Object.entries(rawTileList)) {
+//   for(let multiples = 0; multiples < tileData["tiles"]; multiples++) {
+//     fullTileList[tileId] = [tileData["letter"], tileData["points"], tileId];
+//     tileId++;
+//   }
+// }
 
 //console.log(getRandom(fullTileList, 7));
 
