3b288470cfc44d0e5bf76d95efc1004532aed9b8
Radian758 <adrianvaz09@gmail.com>
2022-12-04 15:51:43 -0800

    rename user file

--- db/users.js
+++ db/users.js
@@ -6,6 +6,7 @@ const REGISTER_USER = "INSERT INTO users (username, password, email) VALUES (${u
 const LOGIN_USER = "SELECT id, username, password FROM users WHERE username=${username}";
 
 const register = ({username, password, email}) => {
+  console.log(username, password, email);
    return db
    .none(LOOKUP_USER_BY_USERNAME, {username})
    .then(() => bcrypt.hash(password, 10))
